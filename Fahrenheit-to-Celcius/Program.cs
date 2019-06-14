0eacfb7ac066838b412c69da7f31a15754dac0e7
Steve Terepin <steve.terepin@gmai.com>
2019-06-14 18:39:05 +0100

    Some cosmetic changes (whitespace)

--- Fahrenheit-to-Celcius/Program.cs
+++ Fahrenheit-to-Celcius/Program.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+using System ;
+using BenchmarkDotNet.Attributes ;
+using BenchmarkDotNet.Running ;
 
 namespace SimpleBenchmark
 {
 
     public class Program
     {
-        public static void Main(string[] args)
+
+        public static void Main ( string[] args )
         {
             var summary_decimal = BenchmarkRunner.Run<Chaining_decimal.Benchmark>();
             var summary_double = BenchmarkRunner.Run<Chaining_double.Benchmark>();
         }
+
     }
+
 }
